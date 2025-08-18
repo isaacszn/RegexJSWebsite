5850c53013091d898bbe7b7f695ce1f23fe40370
Zik Szn <isaaczik9@gmail.com>
2025-08-18 05:07:58 -0700

    little update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
 const Hero = () => {
     return (
         <>
-            <h1 className="hero">RegexJS an Open Source JavaScript library built to clear the worries of developers having to write regular expressions</h1>
+            <div>
+                <h1 className="hero">RegexJS an Open Source JavaScript library built to clear the worries of developers having to write regular expressions</h1>
+                <button className="hero-btn">Get started 🚀</button>
+            </div>
         </>
     )
 }
