4e02b1a86e8ddc5af21a85dbc9a65371f51b8b3d
Zik Szn <isaaczik9@gmail.com>
2025-08-20 09:22:21 -0700

    feat: update intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ const Introduction = () => {
         <>
             <h1 className="sub-header">Introduction</h1>
             <p className="box">
-                What is RegexJS? Regex.js is an Open Source JavaScript library built and maintained by a Nigerian developer name <a href="https://x.com/zikk_szn">Isaac Ugochukwu</a>, to solve the problems of developers having to create regular expression patterns for data/input validation on their amazing projects. <br /><br />
+                What is RegexJS? Regex.js is an Open Source JavaScript library built and maintained by a Nigerian developer named <a href="https://x.com/zikk_szn">Isaac Ugochukwu</a>, to solve the problem of developers having to create regular expression patterns for data/input validation on their amazing projects. <br /><br />
                 RegexJS is also open to contributions, for bug fixes & improvements! Incase you're looking forward to contribute to the project (RegexJS), head to &rarr; <a href="https://github.com/isaacszn/RegexJS">The Repo</a> (And please, don't forget to star the repo :)). Or reach me out on <a href="https://wa.me/+2349166608124">WhatsApp</a>!
             </p>
         </>
