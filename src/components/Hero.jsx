3a27aef5e4681e2d4b3560d5e5ba982b034ccc9c
Zik Szn <isaaczik9@gmail.com>
2025-08-21 03:42:32 -0700

    minor change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 const Hero = () => {
+    const goToGetStartedSec = () => {
+        location.href = "#get-started"
+    }
+
     return (
         <>
             <div>
                 <h1 className="hero">RegexJS an Open Source JavaScript library built to clear the worries of developers having to write regular expressions</h1>
-                <button className="hero-btn">Get started 🚀</button>
+                <button className="hero-btn" onClick={goToGetStartedSec}>Get started 🚀</button>
             </div>
         </>
     )
