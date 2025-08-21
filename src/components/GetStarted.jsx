4db165b89a67795d2e188cb726b8908ac90179e7
Zik Szn <isaaczik9@gmail.com>
2025-08-21 03:38:53 -0700

    small commits to the get started section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,6 +2,8 @@ const GetStarted = () => {
     return (
         <>
             <h1 className="sub-header">Get Started</h1>
+            <h2>Want to start building your project with RegexJS? Here's a guide on how you can setup RegexJS and embed it on your amazing projects:</h2>
+            <p>You can add RegexJS to your project via CDN, by adding the CDN link, <code>https://regexjs.vercel.app/lib/regex.min.js</code> to the script tag in your HTML file in your codebase &rarr; <code title="https://regexjs.vercel.app/lib/regex.min.js"><span className="pink">&lt;script</span> <span className="green">src</span>=<span className="light-green">"https://regexjs.vercel.app/lib/regex.min.js"</span><span className="pink">&gt;</span><span className="pink">&lt;/script&gt;</span></code></p>
         </>
     )
 }
