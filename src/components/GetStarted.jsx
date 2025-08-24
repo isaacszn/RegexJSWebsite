62f2a70f3fcdae3ab10f7231e66cb08d612b8efd
Zik Szn <isaaczik9@gmail.com>
2025-08-24 02:46:15 -0700

    huge changes on parts of the project (most especially the nav section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,12 +1,16 @@
 import SampleCodes from '../components/SampleCodes'
 
 const GetStarted = () => {
+    const style = {
+        margin: "5px"
+    }
+
     return (
         <>
-            <div id="get-started">
-                <h1 className="sub-header">Get Started</h1>
+            <div id="get-started" className="box">
+                <h1 className="sub-header" id='#get-started'>Get Started</h1>
                 <h2>Want to start building your project with RegexJS? Here's a guide on how you can setup RegexJS and embed it on your amazing projects:</h2>
-                <p>You can add RegexJS to your project via CDN, by adding the CDN link, <code>https://regexjs.vercel.app/lib/regex.min.js</code> to the script tag in your HTML file in your codebase &rarr; <code title="https://regexjs.vercel.app/lib/regex.min.js"><span className="pink">&lt;script</span> <span className="green">src</span>=<span className="light-green">"https://regexjs.vercel.app/lib/regex.min.js"</span><span className="pink">&gt;</span><span className="pink">&lt;/script&gt;</span></code></p><br />
+                <p>You can add RegexJS to your project via CDN, by adding the CDN link, <code className="script">https://regexjs.vercel.app/lib/regex.min.js</code> to the script tag in your HTML file in your codebase &rarr; <br /><code className="script" title="https://regexjs.vercel.app/lib/regex.min.js"><span className="pink">&lt;script</span><span style={style} className="green">src</span>=<span className="light-green">"https://regexjs.vercel.app/lib/regex.min.js"</span><span className="pink">&gt;</span><span className="pink">&lt;/script&gt;</span></code></p><br />
                 <p><span className='pink'>*</span>Adding RegexJS via <span className="pink">NPM</span> isn't yet available right now</p><br />
                 Now you have successfully installed & setup RegexJS in your project directory 🎉🎉!!!
             </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,42 @@
+import Footer from '../components/Footer';
+
 const NavBar = () => {
+    const showMenu  = () => {
+        const menu = document.querySelector('.menu');
+        menu.classList.remove('hide');
+        menu.classList.add('show');
+    }
+
+    const hideMenu  = () => {
+        const menu = document.querySelector('.menu');
+        menu.classList.remove('show');
+        menu.classList.add('hide');
+    }
+
     return (
         <>
-            <div className="nav-bar">
+            <div className="nav-bar-mobile">
                 <a href="https://regexjs-doc.vercel.app"><img src='./src/assets/logo.jpg' alt="Logo" width="1024" height="1024"/> <span className="flex">RegexJS</span></a>
-                <span className="menu-btn">&#9776;</span>
+                <span className="menu-btn" onClick={showMenu}>&#9776;</span>
             </div>
+            <div className="nav-bar-desktop">
+                <a href="https://regexjs-doc.vercel.app" className="logo"><img src='./src/assets/logo.jpg' alt="Logo" width="1024" height="1024"/> <span className="flex">RegexJS</span></a>
+                <ul className="nav-links">
+                    <li><a href="#intro">Introduction</a></li>
+                    <li><a href="#get-started">Get started</a></li>
+                    <li><a href="#sample-codes">Sample codes</a></li>
+                </ul>
+            </div>
+            <aside className="menu hide">
+                <span className="hide-btn" onClick={hideMenu}>x</span>
+                <a href="https://regexjs-doc.vercel.app" className="menu-header"><img src='./src/assets/logo.jpg' alt="Logo" width="1024" height="1024"/> <span className="flex">RegexJS</span></a>
+                <ul className="menu-links">
+                    <li><a href="#intro">Introduction</a></li>
+                    <li><a href="#get-started">Get started</a></li>
+                    <li><a href="#sample-codes">Sample codes</a></li>
+                </ul>
+                <Footer />
+            </aside>
         </>
     )
 }
